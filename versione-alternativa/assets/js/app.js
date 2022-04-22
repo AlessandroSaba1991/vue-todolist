99690fc72e16494bd649ac1b7b9306532defddc8
AlessandroSaba1991 <sabaalessandro515@gmail.com>
2022-04-22 17:04:41 +0200

    add versione alternativa fino alla colonna complete

---
+++ versione-alternativa/assets/js/app.js
@@ -0,0 +1,97 @@
+/* 
+DESCRIZIONE:
+## Istruzioni
+Create una todo list usando VueJS.
+Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS.
+Se non sapere che fare, di seguito trovate uno screenshot.
+Funzionalitá:
+- La nostra todo list avrá alcune tasks di default predefinite
+- L'utente puó inserire nuove tasks
+- Cliccando sulla "X" l'utente puó cancellare una task
+- Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+- L'utente vuole poter indicare che la task é stata completata (con un icona cliccabile)
+Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+Da tenere a mente:
+Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari.
+Consultate la documentazione e se siete bloccati, aprite un ticket.
+## Bonus-extra (opzionale)
+- Quando una task é stata completa allora l'utente vuole che venga inserita in un'altra colonna tipo "tasks completate"
+- se una task é stata marcata come completa per sbaglio allora vuole poterla rimettere nella todo list (cliccando su un altra icona)
+- ah non é finita, dice che quando cancella una task non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
+- si, l'utente é un rompi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
+Il nostro utente per ora sembra non avere altre richieste ... ma chissá se dopo gli viene in mente che vuole anche poter rimettere una task cancellata nella lista di tasks da fare, magari l'ha cancellata per sbaglio...
+*/
+const app = new Vue({ 
+    el: "#app", 
+    data: {
+        todos:[
+            {
+                text:'Cammina',
+                done:true
+            },
+            {
+                text:'Corri',
+                done:true
+            },
+            {
+                text:'Mangia',
+                done:false
+            },
+            {
+                text:'Respira',
+                done:true
+            },
+            {
+                text:'Saluta',
+                done:false
+            },
+            {
+                text:'Rotola',
+                done:false
+            },
+        ],
+        todos_complete:[],
+        todos_remove:[],
+        new_todo:{
+            text:'',
+            done:false
+        }
+    }, 
+    methods: {
+        delete_todo(index){
+            this.todos.splice(index,1)
+        },
+        add_todo(){
+            this.todos.push(this.new_todo)
+            this.new_todo = 
+            {
+                text:'',
+                done:false
+            } 
+
+        },
+        change_done(index){
+            this.todos[index].done = true
+            this.true_false()
+        },
+        todo_return(complete, index){
+            this.todos_complete[index].done = false
+            this.todos.push(complete)
+            this.true_false()
+        },
+        true_false(){
+            this.todos.forEach(todo => {
+                if(todo.done === true){
+                    this.todos_complete.push(todo)
+                    this.todos = this.todos.filter(element => element.done !== true)
+                } else {                    
+                    this.todos_complete = this.todos_complete.filter(element => element.done !== false)
+                }
+            })
+        }
+    },
+    mounted(){
+        this.true_false()
+    } 
+});
